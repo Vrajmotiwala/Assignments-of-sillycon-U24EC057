b78cd29897e0b0f66af049c42c9ff95704815d59
unknown <vrajmotiwala@gmail.com>
2025-06-23 22:48:25 +0530

    updated

--- BCD_counter/design.v
+++ BCD_counter/design.v
@@ -13,11 +13,13 @@ module T_flipflop(input T,input CLK,input RES ,output reg Q );
 endmodule
 
 module BCD_counter(input CLK ,output Q1,output Q2 , output Q3, output Q4);
-  wire R;
-  nand(R,Q4,Q2);
-  T_flipflop f1(1'b1,Q3,R,Q4);
-  T_flipflop f2(1'b1,Q2,R,Q3);
-  T_flipflop f3(1'b1,Q1,R,Q2);
-  T_flipflop f4(1'b1,CLK,R,Q1);
+  wire R,A,Z,Y;
+  and(R,Q1);
+  and(A,Q2,Q1);
+  and(Z,Q3,Q2,Q1);
+  nand(Y,Q4,Q2);
+  T_flipflop f1(Z,CLK,Y,Q4);
+  T_flipflop f2(A,CLK,Y,Q3);
+  T_flipflop f3(R,CLK,Y,Q2);
+  T_flipflop f4(1'b1,CLK,Y,Q1);
 endmodule
-  
