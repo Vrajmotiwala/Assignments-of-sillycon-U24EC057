55f2e9acf3397807e9df9622cc3a7c8309ee604e
unknown <vrajmotiwala@gmail.com>
2025-07-03 19:09:41 +0530

    Updated

--- Univeral_ShiftRegister/testbench.v
+++ Univeral_ShiftRegister/testbench.v
@@ -1,45 +1,63 @@
-// Code your design here
-// Code your design here
-module D_flipflop(input D,input CLK,input RES ,output reg Q );
-  initial Q=0;
-  always @(negedge CLK or negedge RES) begin
-    if (!RES) begin
-      Q<=0;
-    end else 
-      Q<=D;
- end
-endmodule
+// Code your testbench here
+// or browse Examples
+module testbench;
+  reg CLK, RES, si, S0, S1;
+  reg I0, I1, I2, I3;
+  wire Q0, Q1, Q2, Q3;
 
+  uni_shiftregister uut (
+    .I0(I0), .I1(I1), .I2(I2), .I3(I3),
+    .CLK(CLK), .RES(RES), .si(si),
+    .S0(S0), .S1(S1),
+    .Q0(Q0), .Q1(Q1), .Q2(Q2), .Q3(Q3)
+  );
 
-module mux(input M0, input M1, input M2, input M3, input S0, input S1, output reg T);
-  always @(*) begin
-    case ({S1, S0})
-      2'b00: T = M0;
-      2'b01: T = M1;
-      2'b10: T = M2;
-      2'b11: T = M3;
-      default: T = 1'b0;
-    endcase
+  initial 
+    CLK = 0;
+  always #5 CLK=~CLK;
+  
+  initial begin
+    RES=0;
+    S0=0;
+    S1=0;
+    I0=0;
+    I1=0;
+    I2=0;
+    I3=0;
+    si=0;
   end
-endmodule
+    
+    
 
-      
-module uni_shiftregister(input I0 , input I1, input I2, input I3, input CLK,input RES,input si,input S0,input S1, output Q0,Q1,Q2, Q3);
-  wire [3:0] A;
-  wire Q0_int, Q1_int, Q2_int, Q3_int ;
-  mux m0(Q0_int,si,Q1_int,I0,S0,S1,A[0]);
-  mux m1(Q1_int,Q0_int,Q2_int,I1,S0,S1,A[1]);
-  mux m2(Q2_int,Q1_int,Q3_int,I2,S0,S1,A[2]);
-  mux m3(Q3_int,Q2_int,si,I3,S0,S1,A[3]);
-  
-  D_flipflop f0(A[0],CLK,RES,Q0_int);
-  D_flipflop f1(A[1],CLK,RES,Q1_int);
-  D_flipflop f2(A[2],CLK,RES,Q2_int);
-  D_flipflop f3(A[3],CLK,RES,Q3_int);
-  
-  assign Q0=Q0_int;
-  assign Q1=Q1_int;
-  assign Q2=Q2_int;
-  assign Q3=Q3_int;
+  initial begin
+    $dumpfile("testbench.vcd");
+    $dumpvars(0,testbench);
+ 
+     // Reset-sync
+  RES = 0;
+  @(negedge CLK);
+  RES = 1;
+  @(negedge CLK);
+
+  // Parallel Load
+  I0=1; I1=1; I2=0; I3=1; S1=1; S0=1;
+  @(negedge CLK);
+
+  // Hold
+  S1=0; S0=0;
+  @(negedge CLK);
+
+  // Shift Right
+  si=0; S1=0; S0=1;
+  @(negedge CLK);
+
+  // Shift Left
+  si=1; S1=1; S0=0;
+  @(negedge CLK);
+
+  // Final Hold
+  S1=0; S0=0;
+  @(negedge CLK);
+  $finish;
+end
 endmodule
-      
